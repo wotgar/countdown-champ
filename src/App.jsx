189ab59f24414bc720e0f4c29bd741da7251db6c
Georg Sibinger <wotgar@gmx.de>
2018-06-29 21:43:57 +0200

    Minor Changes in CSS and Form Handling

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,21 @@
 import React, { Component } from 'react';
 import Clock from './Clock.jsx';
 import './App.css';
-import { Form, FormControl, Button } from 'react-bootstrap';
+import { FormGroup, InputGroup, FormControl, Button } from 'react-bootstrap';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       deadline: 'December 25, 2018',
-      newDeadline : ' '
+      newDeadline : ''
     }
   }
 
   changeDeadLine() {
       console.log('state', this.state);
       this.setState({deadline: this.state.newDeadline});
+      this.setState({newDeadline: ''});
   }
 
   render() {
@@ -25,16 +26,31 @@ class App extends Component {
           deadline={this.state.deadline}
         />
       {/* Hier werden nun die oben importierten bootstrap Klassen verwendet */}
-        <Form inline={true}>
-        {/* Im folgenden verwenden wir zwei anonymous functions; sie dienen zur Sicherheit vor loops */}
-          <FormControl
-            placeholder='new deadline'
-            onChange={event => this.setState({newDeadline: event.target.value})}
-          />
-        <Button onClick={() => this.changeDeadLine()} >
-            Submit
-          </Button>
-        </Form>
+        <FormGroup>
+          <InputGroup
+            className="Deadline-group">
+          {/* Im folgenden verwenden wir zwei anonymous functions; sie dienen zur Sicherheit vor loops */}
+            <FormControl
+              className="Deadline-input"
+              value={this.state.newDeadline}
+              type="text"
+              placeholder='new deadline'
+              onChange={event => this.setState({newDeadline: event.target.value})}
+              onKeyPress={event => {
+                  if (event.key === 'Enter') {
+                    this.changeDeadLine()
+                  }}}
+              preventDefault
+            />
+            <InputGroup.Button>
+              <Button
+                className="Deadline-button"
+                onClick={() => this.changeDeadLine()}>
+                submit
+              </Button>
+            </InputGroup.Button>
+          </InputGroup>
+        </FormGroup>
       </div>
     )
   }
